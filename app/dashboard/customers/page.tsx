6de7905f63a663809bb06c960805c21cb55da21f
Rakhmiddin Asiydinov <rahmiddinasiydinov@gmail.com>
2025-08-11 12:34:08 +0500

    start next

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Page({}: Props) {
+  return (
+    <div>Customers Page</div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Page({}: Props) {
+  return (
+    <div>Invoices Page</div>
+  )
+}
+
+export default Page
